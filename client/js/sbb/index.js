e74db3450889fd9932dc4360140edc690fe603df
ue62793 <michael.rudiger@sbb.ch>
2016-06-08 13:52:42 +0200

    mru: module index.js must have return

--- client/js/sbb/index.js
+++ client/js/sbb/index.js
@@ -3,20 +3,22 @@ define(function (require) {
 	var $ = require('jquery');
 	var datatables =  require('datatables');
 	var IndexPageDataSetGenerated = require('indexPageDataSet.js');
-	$(document).ready(function() {
-		var indexPageData = new IndexPageDataSetGenerated();
-		$('#build-status-index').dataTable( {
-			"paging": false,
-			"data": indexPageData.indexPageDataSet,
-			"columns": [
-				{title: "Snapshot"},
-				{title: "Stabil"},
-				{title: "Edu-T"},
-				{title: "Test"},
-				{title: "Integration"},
-				{title: "Schulung"},
-				{title: "Produktion"},
-			]
+	return function () {
+		$(document).ready(function() {
+			var indexPageData = new IndexPageDataSetGenerated();
+			$('#build-status-index').dataTable( {
+				"paging": false,
+				"data": indexPageData.indexPageDataSet,
+				"columns": [
+					{title: "Snapshot"},
+					{title: "Stabil"},
+					{title: "Edu-T"},
+					{title: "Test"},
+					{title: "Integration"},
+					{title: "Schulung"},
+					{title: "Produktion"},
+				]
+			});
 		});
-	});
+	};
 });
