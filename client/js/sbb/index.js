7f91bb1bb411358a1de2cd9560c96f9a20aae6f6
ue62793 <michael.rudiger@sbb.ch>
2016-06-08 11:28:37 +0200

    mru: handle stuff with requireJs

--- client/js/sbb/index.js
+++ client/js/sbb/index.js
@@ -1,15 +1,19 @@
-$(document).ready(function() {
-	$('#build-status-index').DataTable( {
-		"paging": false,
-		"data": indexPageDataSet,
-		"columns": [
-			{title: "Snapshot"},
-			{title: "Stabil"},
-			{title: "Edu-T"},
-			{title: "Test"},
-			{title: "Integration"},
-			{title: "Schulung"},
-			{title: "Produktion"},
-		]
+define(function (require) {
+	var IndexPageDataSet = require('indexPageDataSet.js');
+	var indexPageData = new IndexPageDataSet();
+	$(document).ready(function() {
+		$('#build-status-index').DataTable( {
+			"paging": false,
+			"data": indexPageData.indexPageDataSet,
+			"columns": [
+				{title: "Snapshot"},
+				{title: "Stabil"},
+				{title: "Edu-T"},
+				{title: "Test"},
+				{title: "Integration"},
+				{title: "Schulung"},
+				{title: "Produktion"},
+			]
+		});
 	});
 });
