89017173bb7b7745635ab3e22ec1be8e9cb214ab
ue62793 <michael.rudiger@sbb.ch>
2016-06-14 13:58:00 +0200

    mru: trying to create links

--- client/js/sbb/index.js
+++ client/js/sbb/index.js
@@ -2,36 +2,29 @@ define(function (require) {
 	// libs
 	var $ = require('jquery');
 	var datatables =  require('datatables');
+	var IndexPageDataSetGenerated = require('indexPageDataSet.js');
 	return function () {
 		$(document).ready(function() {
-			$('#build-status-index').DataTable( {
+			$('#build-status-index').dataTable( {
 				"paging": false,
-				"searching": false,
 				"processing": true,
-				"jQueryUI": true,
-				"autoWidth": false,
 				"ajax": "indexPageData.json",
 				"columns": [
-					{"title": "Mega/Stage", "data": "mega"},
-					{"title": "Snapshot", "data": "snapshot"},
-					{"title": "Stabil", "data": "stabil"},
-					{"title": "Edut", "data": "edut"},
-					{"title": "Test", "data": "test"},
-					{"title": "Integration", "data": "integration"},
-					{"title": "Schulung", "data": "schulung"},
-					{"title": "Produktion", "data": "produktion"},
+					{title: "Mega/Stage", data: "mega"},
+					{title: "Snapshot", data: "snapshot"},
+					{title: "Stabil", data: "stabil"},
+					{title: "Edut", data: "edut"},
+					{title: "Test", data: "test"},
+					{title: "Integration", data: "integration"},
+					{title: "Schulung", data: "schulung"},
+					{title: "Produktion", data: "produktion"},
 				],
-				"columnDefs": [
-					{"targets": '_all', "defaultContent": "-" },
+				columnDefs: [
+					{targets: '_all', defaultContent: "-" },
 					{
-					"targets": '_all',
-					"render": function(data, type, row) {
-							var retVal = data;
-							if (type === "display" && data != "-") {
-								retVal =  '<a href="' + row.mega + '/' + data + "/" + data + '">' + data + '</a>';
-							}
-							return retVal;
-						}
+					"targets": 0,
+					"data": "download_link",
+					"render": function ( data, type, full, meta ) {return '<a href="'+data+'">Download</a>';}
 					}
 				]
 			});
